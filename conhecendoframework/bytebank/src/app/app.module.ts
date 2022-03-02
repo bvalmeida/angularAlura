2401812d87d382c762bf16909f87a232edf3f457
bvalmeida <bvalmeida94@gmail.com>
2022-03-02 13:17:27 -0300

    primeiro

---
+++ conhecendoframework/bytebank/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NovaTransferenciaComponent } from './novatransferencia/novatransferencia.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ conhecendoframework/bytebank/src/app/novatransferencia/novatransferencia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'appnovatransferencia',
+    templateUrl: './novatransferencia.component.html',
+    styleUrls: ['./novatransferencia.component.scss']
+})
+export class NovaTransferenciaComponent {
+
+}
