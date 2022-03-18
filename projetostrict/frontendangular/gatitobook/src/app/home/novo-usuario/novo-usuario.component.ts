122a0f2d48982997e192d5f9b86c9179132bf838
bvalmeida <bvalmeida94@gmail.com>
2022-03-18 14:31:43 -0300

    utilizando formulário reativo e aplicando algumas valicações no cadastro

--- projetostrict/frontendangular/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ projetostrict/frontendangular/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,8 @@
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  novoUsuarioForm!:FormGroup;
+
+  constructor(private formBuilder:FormBuilder, private novoUsuarioService:NovoUsuarioService) { }
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email:['', [
+        Validators.required,
+        Validators.email
+      ]],
+      fullName:['', [
+        Validators.required,
+        Validators.minLength(4)
+      ]],
+      userName:['', [
+        minusculoValidator
+      ]],
+      password:['']
+    });
+  }
+
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
   }
 
 }
