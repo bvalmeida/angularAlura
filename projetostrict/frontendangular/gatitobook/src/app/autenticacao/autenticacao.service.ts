d886bd11a97b23be4f02dbce35b0b0c5dfada842
bvalmeida <bvalmeida94@gmail.com>
2022-03-29 14:33:31 -0300

    progredindo em angular

--- projetostrict/frontendangular/gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ projetostrict/frontendangular/gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,26 +1,35 @@
+import { environment } from './../../environments/environment';
 import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 
+const API = environment.apiURL;
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor( private httpClient:HttpClient, private usuarioService:UsuarioService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autentica(ususario:string, senha:string):Observable<HttpResponse<any>>{
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: ususario,
-      password: senha,
-    },
-    {observe:'response'}
-    ).pipe(
-      tap((res) => {
-        const authToken = res.headers.get('x-access-token') ?? '';
-        this.usuarioService.salvaToken(authToken);
-      })
-    )
+  autentica(ususario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        `${API}/user/login`,
+        {
+          userName: ususario,
+          password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
-
 }
